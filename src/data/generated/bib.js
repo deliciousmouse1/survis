076e5fa28bd96e9b7fd4fda54b3a005fe9f17123
deliciousmouse1 <339631039@qq.com>
2026-05-01 21:55:09 +0800

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,159 +1,156 @@
-﻿const generatedBibEntries = {
+const generatedBibEntries = {
 
 "Chen2024MMHCA": {
-"title": "MMHCA: Multi-feature representations based on multi-scale hierarchical contextual aggregation for UAV-view geo-localization",
-"author": "Chen, Nanhua and Lou, Tai-Shan and Zhao, Liangyu",
-"year": "2024",
-"journal": "Chinese Journal of Aeronautics",
-"series": "Chinese Journal of Aeronautics",
-"doi": "10.1016/j.cja.2024.09.018",
-"keywords": "type:method, topic:multi-scale, topic:contextual-aggregation, application:UAV-view-geo-localization"
+  "title": "MMHCA: Multi-feature representations based on multi-scale hierarchical contextual aggregation for UAV-view geo-localization",
+  "author": "Chen, Nanhua and Lou, Tai-Shan and Zhao, Liangyu",
+  "year": "2024",
+  "journal": "Chinese Journal of Aeronautics",
+  "doi": "10.1016/j.cja.2024.09.018",
+  "keywords": "type:method, topic:multi-scale, topic:contextual-aggregation, application:UAV-view-geo-localization"
 },
 
 "Dai2022FSRA": {
-"title": "A Transformer-Based Feature Segmentation and Region Alignment Method for UAV-View Geo-Localization",
-"author": "Dai, Ming and Hu, Jian and Zhuang, Jiedong and Zheng, Enhui",
-"year": "2022",
-"journal": "IEEE Transactions on Circuits and Systems for Video Technology",
-"series": "IEEE TCSVT",
-"volume": "32",
-"number": "7",
-"pages": "4376--4389",
-"doi": "10.1109/TCSVT.2021.3135013",
-"keywords": "type:method, topic:transformer, topic:region-alignment, application:UAV-view-geo-localization"
+  "title": "A Transformer-Based Feature Segmentation and Region Alignment Method for UAV-View Geo-Localization",
+  "author": "Dai, Ming and Hu, Jian and Zhuang, Jiedong and Zheng, Enhui",
+  "year": "2022",
+  "journal": "IEEE Transactions on Circuits and Systems for Video Technology",
+  "volume": "32",
+  "number": "7",
+  "pages": "4376--4389",
+  "doi": "10.1109/TCSVT.2021.3135013",
+  "keywords": "type:method, topic:transformer, topic:region-alignment, application:UAV-view-geo-localization"
 },
 
 "Deuser2023Sample4Geo": {
-"title": "Sample4Geo: Hard negative sampling for cross-view geo-localisation",
-"author": "Deuser, Fabian and Habel, Konrad and Oswald, Norbert",
-"year": "2023",
-"booktitle": "Proceedings of the IEEE/CVF International Conference on Computer Vision",
-"series": "ICCV",
-"pages": "16847--16856",
-"doi": "10.1109/ICCV51070.2023.01545",
-"keywords": "type:method, topic:contrastive-learning, topic:hard-negative-sampling, application:cross-view-geo-localisation"
+  "title": "Sample4Geo: Hard negative sampling for cross-view geo-localisation",
+  "author": "Deuser, Fabian and Habel, Konrad and Oswald, Norbert",
+  "year": "2023",
+  "booktitle": "Proceedings of the IEEE/CVF International Conference on Computer Vision",
+  "pages": "16847--16856",
+  "doi": "10.1109/ICCV51070.2023.01545",
+  "keywords": "type:method, topic:contrastive-learning, topic:hard-negative-sampling, application:cross-view-geo-localisation"
 },
 
 "Ge2024Multibranch": {
-"title": "Multibranch Joint Representation Learning Based on Information Fusion Strategy for Cross-View Geo-Localization",
-"author": "Ge, Fawei and Zhang, Yunzhou and Liu, Yixiu and Wang, Guiyuan and Coleman, Sonya and Kerr, Dermot and Wang, Lipo",
-"year": "2024",
-"journal": "IEEE Transactions on Geoscience and Remote Sensing",
-"series": "IEEE TGRS",
-"volume": "62",
-"pages": "1--16",
-"doi": "10.1109/TGRS.2024.3378453",
-"keywords": "type:method, topic:multibranch, topic:information-fusion, application:cross-view-geo-localization"
+  "title": "Multibranch Joint Representation Learning Based on Information Fusion Strategy for Cross-View Geo-Localization",
+  "author": "Ge, Fawei and Zhang, Yunzhou and Liu, Yixiu and Wang, Guiyuan and Coleman, Sonya and Kerr, Dermot and Wang, Lipo",
+  "year": "2024",
+  "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+  "volume": "62",
+  "pages": "1--16",
+  "doi": "10.1109/TGRS.2024.3378453",
+  "keywords": "type:method, topic:multibranch, topic:information-fusion, application:cross-view-geo-localization"
 },
 
 "Gong2024SatelliteDrone": {
-"title": "A Satellite-Drone Image Cross-View Geolocalization Method Based on Multi-Scale Information and Dual-Channel Attention Mechanism",
-"author": "Gong, Naigun and Li, Liwei and Sha, J. and Sun, Xu and Huang, Qian",
-"year": "2024",
-"journal": "Remote Sensing",
-"series": "Remote Sensing",
-"volume": "16",
-"number": "6",
-"pages": "941",
-"doi": "10.3390/rs16060941",
-"keywords": "type:method, topic:multi-scale, topic:attention, application:satellite-drone-geolocalization"
+  "title": "A Satellite-Drone Image Cross-View Geolocalization Method Based on Multi-Scale Information and Dual-Channel Attention Mechanism",
+  "author": "Gong, Naigun and Li, Liwei and Sha, J. and Sun, Xu and Huang, Qian",
+  "year": "2024",
+  "journal": "Remote Sensing",
+  "volume": "16",
+  "number": "6",
+  "pages": "941",
+  "doi": "10.3390/rs16060941",
+  "keywords": "type:method, topic:multi-scale, topic:attention, application:satellite-drone-geolocalization"
 },
 
 "He2024MSMTransformer": {
-"title": "A contrastive learning based multiview scene matching method for UAV view geo-localization",
-"author": "He, Qiyi and Xu, Ao and Zhang, Yifan and Ye, Zhiwei and Zhou, Wen and Xi, Ruijie and Lin, Qiao",
-"year": "2024",
-"journal": "Remote Sensing",
-"series": "Remote Sensing",
-"volume": "16",
-"number": "16",
-"pages": "3039",
-"doi": "10.3390/rs16163039",
-"keywords": "type:method, topic:transformer, topic:contrastive-learning, application:UAV-view-geo-localization"
+  "title": "A contrastive learning based multiview scene matching method for UAV view geo-localization",
+  "author": "He, Qiyi and Xu, Ao and Zhang, Yifan and Ye, Zhiwei and Zhou, Wen and Xi, Ruijie and Lin, Qiao",
+  "year": "2024",
+  "journal": "Remote Sensing",
+  "volume": "16",
+  "number": "16",
+  "pages": "3039",
+  "doi": "10.3390/rs16163039",
+  "keywords": "type:method, topic:contrastive-learning, application:UAV-view-geo-localization"
 },
 
 "Li2024GeoFormer": {
-"title": "GeoFormer: An Effective Transformer-Based Siamese Network for UAV Geolocalization",
-"author": "Li, Qingge and Yang, Xiaogang and Fan, Jiwei and Lu, Ruitao and Tang, Bin and Wang, Shanshan and Su, Shiliang",
-"year": "2024",
-"journal": "IEEE Journal of Selected Topics in Applied Earth Observations and Remote Sensing",
-"series": "IEEE JSTARS",
-"volume": "17",
-"pages": "9470--9491",
-"doi": "10.1109/JSTARS.2024.3392812",
-"keywords": "type:method, topic:transformer, topic:siamese-network, application:UAV-geolocalization"
+  "title": "GeoFormer: An Effective Transformer-Based Siamese Network for UAV Geolocalization",
+  "author": "Li, Qingge and Yang, Xiaogang and Fan, Jiwei and Lu, Ruitao and Tang, Bin and Wang, Shanshan and Su, Shiliang",
+  "year": "2024",
+  "journal": "IEEE Journal of Selected Topics in Applied Earth Observations and Remote Sensing",
+  "volume": "17",
+  "pages": "9470--9491",
+  "doi": "10.1109/JSTARS.2024.3392812",
+  "keywords": "type:method, topic:transformer, application:UAV-geolocalization"
 },
 
 "Lin2022RKNet": {
-"title": "Joint Representation Learning and Keypoint Detection for Cross-View Geo-Localization",
-"author": "Lin, Jinliang and Zheng, Zhedong and Zhong, Zhun and Luo, Zhiming and Li, Shaozi and Yang, Yi and Sebe, Nicu",
-"year": "2022",
-"journal": "IEEE Transactions on Image Processing",
-"series": "IEEE TIP",
-"volume": "31",
-"pages": "3780--3792",
-"doi": "10.1109/TIP.2022.3175601",
-"keywords": "type:method, topic:keypoint-detection, topic:representation-learning, application:cross-view-geo-localization"
+  "title": "Joint Representation Learning and Keypoint Detection for Cross-View Geo-Localization",
+  "author": "Lin, Jinliang and Zheng, Zhedong and Zhong, Zhun and Luo, Zhiming and Li, Shaozi and Yang, Yi and Sebe, Nicu",
+  "year": "2022",
+  "journal": "IEEE Transactions on Image Processing",
+  "volume": "31",
+  "pages": "3780--3792",
+  "doi": "10.1109/TIP.2022.3175601",
+  "keywords": "type:method, topic:representation-learning, application:cross-view-geo-localization"
 },
 
 "Wang2022EachPart": {
-"title": "Each Part Matters: Local Patterns Facilitate Cross-View Geo-Localization",
-"author": "Wang, Tingyu and Zheng, Zhedong and Yan, Chenggang and Zhang, Jiyong and Sun, Yaoqi and Zheng, Bolun and Yang, Yi",
-"year": "2022",
-"journal": "IEEE Transactions on Circuits and Systems for Video Technology",
-"series": "IEEE TCSVT",
-"volume": "32",
-"number": "2",
-"pages": "867--879",
-"doi": "10.1109/TCSVT.2021.3061265",
-"keywords": "type:method, topic:local-patterns, topic:spatial-partitioning, application:cross-view-geo-localization"
+  "title": "Each Part Matters: Local Patterns Facilitate Cross-View Geo-Localization",
+  "author": "Wang, Tingyu and Zheng, Zhedong and Yan, Chenggang and Zhang, Jiyong and Sun, Yaoqi and Zheng, Bolun and Yang, Yi",
+  "year": "2022",
+  "journal": "IEEE Transactions on Circuits and Systems for Video Technology",
+  "volume": "32",
+  "number": "2",
+  "pages": "867--879",
+  "doi": "10.1109/TCSVT.2021.3061265",
+  "keywords": "type:method, topic:local-patterns, application:cross-view-geo-localization"
 },
 
 "Wu2024CAMP": {
-"title": "CAMP: A Cross-View Geo-Localization Method Using Contrastive Attributes Mining and Position-Aware Partitioning",
-"author": "Wu, Qiong and Wan, Y. and Zheng, Zhi and Zhang, Yongjun and Wang, G. and Zhao, Z.",
-"year": "2024",
-"journal": "IEEE Transactions on Geoscience and Remote Sensing",
-"series": "IEEE TGRS",
-"volume": "62",
-"pages": "1--14",
-"doi": "10.1109/TGRS.2024.3448499",
-"keywords": "type:method, topic:contrastive-attributes-mining, topic:position-aware-partitioning, application:cross-view-geo-localization"
+  "title": "CAMP: A Cross-View Geo-Localization Method Using Contrastive Attributes Mining and Position-Aware Partitioning",
+  "author": "Wu, Qiong and Wan, Y. and Zheng, Zhi and Zhang, Yongjun and Wang, G. and Zhao, Z.",
+  "year": "2024",
+  "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+  "volume": "62",
+  "pages": "1--14",
+  "doi": "10.1109/TGRS.2024.3448499",
+  "keywords": "type:method, application:cross-view-geo-localization"
 },
 
 "Zhao2024TransFG": {
-"title": "TransFG: A Cross-View Geo-Localization of Satellite and UAVs Imagery Pipeline Using Transformer-Based Feature Aggregation and Gradient Guidance",
-"author": "Zhao, Hu and Ren, Keyan and Yue, Tianyi and Zhang, Chun and Yuan, Shuaidon",
-"year": "2024",
-"journal": "IEEE Transactions on Geoscience and Remote Sensing",
-"series": "IEEE TGRS",
-"volume": "62",
-"pages": "1--12",
-"doi": "10.1109/TGRS.2024.3352418",
-"keywords": "type:method, topic:transformer, topic:feature-aggregation, topic:gradient-guidance, application:satellite-UAV-cross-view-geo-localization"
+  "title": "TransFG: A Cross-View Geo-Localization of Satellite and UAVs Imagery Pipeline Using Transformer-Based Feature Aggregation and Gradient Guidance",
+  "author": "Zhao, Hu and Ren, Keyan and Yue, Tianyi and Zhang, Chun and Yuan, Shuaidon",
+  "year": "2024",
+  "journal": "IEEE Transactions on Geoscience and Remote Sensing",
+  "volume": "62",
+  "pages": "1--12",
+  "doi": "10.1109/TGRS.2024.3352418",
+  "keywords": "type:method, topic:transformer, application:satellite-UAV-cross-view-geo-localization"
 },
 
 "Zheng2020University1652": {
-"title": "University-1652: A Multi-view Multi-source Benchmark for Drone-based Geo-localization",
-"author": "Zheng, Zhedong and Wei, Yunchao and Yang, Yi",
-"year": "2020",
-"booktitle": "Proceedings of the 28th ACM International Conference on Multimedia",
-"series": "ACM MM",
-"pages": "1395--1403",
-"doi": "10.1145/3394171.3413896",
-"keywords": "type:benchmark, dataset:University-1652, application:drone-based-geo-localization"
+  "title": "University-1652: A Multi-view Multi-source Benchmark for Drone-based Geo-localization",
+  "author": "Zheng, Zhedong and Wei, Yunchao and Yang, Yi",
+  "year": "2020",
+  "booktitle": "Proceedings of the 28th ACM International Conference on Multimedia",
+  "pages": "1395--1403",
+  "doi": "10.1145/3394171.3413896",
+  "keywords": "type:benchmark, application:drone-based-geo-localization"
 },
 
 "Zhu2023SUES200": {
-"title": "SUES-200: A Multi-Height Multi-Scene Cross-View Image Benchmark Across Drone and Satellite",
-"author": "Zhu, Runzhe and Yang, Mingze and Wu, Fei and Yang, Yuncheng and Hu, W.",
-"year": "2023",
-"journal": "IEEE Transactions on Circuits and Systems for Video Technology",
-"series": "IEEE TCSVT",
-"volume": "33",
-"number": "9",
-"pages": "4825--4839",
-"doi": "10.1109/TCSVT.2023.3249204",
-"keywords": "type:benchmark, dataset:SUES-200, application:drone-satellite-cross-view"
+  "title": "SUES-200: A Multi-Height Multi-Scene Cross-View Image Benchmark Across Drone and Satellite",
+  "author": "Zhu, Runzhe and Yang, Mingze and Wu, Fei and Yang, Yuncheng and Hu, W.",
+  "year": "2023",
+  "journal": "IEEE Transactions on Circuits and Systems for Video Technology",
+  "volume": "33",
+  "number": "9",
+  "pages": "4825--4839",
+  "doi": "10.1109/TCSVT.2023.3249204",
+  "keywords": "type:benchmark, application:drone-satellite-cross-view"
+},
+"Durgam2024Survey": {
+    "title": "Cross-view geo-localization: A survey",
+    "author": "Durgam, Abhilash and Paheding, Sidike and Dhiman, Vikas and Devabhaktuni, Vijay",
+    "year": "2024",
+    "journal": "IEEE Access",
+    "volume": "12",
+    "pages": "192028--192050",
+    "doi": "10.1109/ACCESS.2024.3476946",
+    "keywords": "type:survey, application:cross-view-geo-localization"
 }
 }});
